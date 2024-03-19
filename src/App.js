782b0d41469cae93cfa5f352101f0004077ba021
Vynokur Yurii <vynokur.yurii@gmail.com>
2024-03-19 21:01:55 +0200

    Add cleanUp function for fetching data

--- src/App.js
+++ src/App.js
@@ -63,11 +63,13 @@ export default function App() {
   const [selectedWatchedMovie, setSelectedWatchedMovie] = useState(null);
 
   useEffect(() => {
+    const controller = new AbortController();
     async function fetchMovies() {
       try {
         setIsLoading(true);
         setError("");
-        const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
+        const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+          {signal: controller.signal});
 
         if (!res.ok) throw new Error("Something went wrong with fetching movies");
 
@@ -76,8 +78,11 @@ export default function App() {
         if (data.Response === 'False') throw new Error("Movie not found");
 
         setMovies(data.Search);
+        setError("");
       } catch (err) {
-        setError(err.message);
+         if (err.name !== "AbortError") {
+           setError(err.message);
+         }
       } finally {
         setIsLoading(false);
       }
@@ -90,6 +95,10 @@ export default function App() {
     }
 
     fetchMovies();
+
+    return function () {
+      controller.abort();
+    }
   }, [query]);
 
   function handleQuery(queryText) {
